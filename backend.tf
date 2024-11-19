05285438659d4bb7035491c7e85ad88692ef0696
Asad Ullah Khan <asad.ullah.khan@macys.com>
2024-11-19 20:52:56 +0500

    check

--- backend.tf
+++ backend.tf
@@ -7,12 +7,7 @@ terraform {
     }
   }
 }
-
 provider "google" {
   impersonate_service_account = "terraform@gcp-hackathon-macys.iam.gserviceaccount.com"
   project                     = var.project_id
 }
-# provider "google-beta" {
-#   impersonate_service_account = "terraform@gcp-hackathon-macys.iam.gserviceaccount.com"
-#   project                     = var.project_id
-# }
