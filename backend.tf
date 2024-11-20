3a02af66660fa87ae2b20f50260f95ac1cc0ba43
Asad Ullah Khan <asad.ullah.khan@macys.com>
2024-11-20 16:35:14 +0500

    added

--- backend.tf
+++ backend.tf
@@ -11,10 +11,18 @@ provider "google" {
 #   impersonate_service_account = "terraform@gcp-hackathon-macys.iam.gserviceaccount.com"
   project                     = var.project_id
   impersonate_service_account = "terraform@gcp-hackathon-macys.iam.gserviceaccount.com"
+    scopes = [
+    "https://www.googleapis.com/auth/cloud-platform",
+    "https://www.googleapis.com/auth/userinfo.email",
+  ]
 }
 
 provider "google-beta" {
 #   impersonate_service_account = "terraform@gcp-hackathon-macys.iam.gserviceaccount.com"
   project                     = var.project_id
   impersonate_service_account = "terraform@gcp-hackathon-macys.iam.gserviceaccount.com"
+    scopes = [
+    "https://www.googleapis.com/auth/cloud-platform",
+    "https://www.googleapis.com/auth/userinfo.email",
+  ]
 }
